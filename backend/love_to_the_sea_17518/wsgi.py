8a11fceb55d9e19e37a0740d9fdf591bdadec903
Crowdbotics <team@crowdbotics.com>
2020-05-28 21:04:26 +0000

    Update models

--- backend/love_to_the_sea_17518/wsgi.py
+++ backend/love_to_the_sea_17518/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'love_to_the_sea_17518.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "love_to_the_sea_17518.settings")
 
 application = get_wsgi_application()
